3dc308a7b2644025bc3dc248a34d0b9fbae276b1
alex <je.portino@duocuc.cl>
2025-04-30 12:36:56 -0400

    first commit

---
+++ denuevo/src/main/java/com/otravez/denuevo/DenuevoApplication.java
@@ -0,0 +1,13 @@
+package com.otravez.denuevo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DenuevoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DenuevoApplication.class, args);
+	}
+
+}
---
+++ denuevo/src/test/java/com/otravez/denuevo/DenuevoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otravez.denuevo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DenuevoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
